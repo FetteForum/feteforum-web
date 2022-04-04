af55a7ff36058ba3e22b0b506cead4623bdfe9e9
FetteForum <102992683+FetteForum@users.noreply.github.com>
2022-04-04 14:41:38 -0400

    Add files via upload

---
+++ listed.js
@@ -0,0 +1,9 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw6dN3ze8F7ZjhxvLJBQUJXNdbHOLDrIsjdxkpG1bhGSZmQ5NOHN59cC7rxJ_F0fPfu3A/exec'
+const form = document.forms['google-sheet']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => alert("Thanks for Contacting us..! We Will Contact You Soon..."))
+    .catch(error => console.error('Error!', error.message))
+})
