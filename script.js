331a3f8451cfdec2272ec974cb33116b30c64d96
FetteForum <102992683+FetteForum@users.noreply.github.com>
2022-04-18 19:54:52 -0400

    Update script.js

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ menu.onclick = () =>{
 }
 
 window.onscroll = () =>{
-    menu.classList.remove('fa-times');
+    menu.classList.remove('fa-times'); 
     navbar.classList.remove('active');
 }
 
@@ -27,7 +27,7 @@ var swiper = new Swiper(".home-slider", {
     //   el: ".swiper-pagination",
     loop:true,
     autoplay:{
-        delay: 2000,
+        delay: 1000,
         disableOnInteraction:false,
     },
   });
